0ce46019ce5ade800b429c69870957ebdb327a5a
wantonraven <josephngalu96@gmail.com>
2023-07-04 14:58:57 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened."""
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened.
+    Args:
+        boxes: list of lists
+    Returns:
+        True if all boxes can be opened, else return False
+    """
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
