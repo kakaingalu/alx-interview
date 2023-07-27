3f950af503fb504db7af3bbc14bdcdadbd95c246
wantonraven <josephngalu96@gmail.com>
2023-07-27 20:41:34 +0300

    remove match case from if statement

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ A method that determines if a given data set represents \
 
 
 def validUTF8(data):
-    """_summary_
+    """Determines if a given data set represents a valid UTF-8 encoding.
 
     Args:
         data (list): a list of integers
@@ -17,15 +17,14 @@ def validUTF8(data):
     con = 0
     for i in data:
         if con == 0:
-            match i:
-                case _ if (i >> 5) == 0b110:
-                    con = 1
-                case _ if (i >> 4) == 0b1110:
-                    con = 2
-                case _ if (i >> 3) == 0b11110:
-                    con = 3
-                case _ if (i >> 7) != 0:
-                    return False
+            if (i >> 5) == 0b110:
+                con = 1
+            elif (i >> 4) == 0b1110:
+                con = 2
+            elif (i >> 3) == 0b11110:
+                con = 3
+            elif (i >> 7) != 0:
+                return False
         else:
             if (i >> 6) != 0b10:
                 return False
