26678ca2ec62a2f247159cb8e6b6a3dfe63685f2
wantonraven <josephngalu96@gmail.com>
2023-07-28 19:57:00 +0300

    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,25 +25,25 @@ file_size = 0
 code = 0
 counter = 0
 dict = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  
-        parsed_line = parsed_line[::-1] 
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
         if len(parsed_line) > 2:
             counter += 1
 
             if counter <= 10:
-                file_size += int(parsed_line[0])  
-                code = parsed_line[1]  
+                file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
                 if (code in dict.keys()):
                     dict[code] += 1
