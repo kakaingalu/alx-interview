0f50abefe1061b4b083d432ecd5d799eedf6affb
wantonraven <josephngalu96@gmail.com>
2023-08-23 08:32:02 +0300

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""generates change
+"""
+
+def makeChange(coins, total):
+    """A function that determines the 
+    fewest number of coins needed to 
+    meet a given amount total
+    
+    Args:
+        coins (_type_): a list of the values 
+        of the coins in your possession
+        total (_type_): total amount
+        
+    return:
+           fewest number of coins needed 
+           to meet total
+    """
+    if total == 0 or total < 0:
+        return 0
+    
+    arr = [float('inf')] * (total + 1)
+    arr[0] = 0
+    
+    for k in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                arr[k] = min(arr[k], 1 + arr[k - coin])
+    
+    if arr[total] == float('inf'):
+        return -1
+    else: 
+        return arr[total]
+    
