1373e4b9cb2edda05858f19d5f8664d3293c861d
wantonraven <josephngalu96@gmail.com>
2023-08-23 08:43:36 +0300

    correct typo

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,7 +22,7 @@ def makeChange(coins, total):
 
     for k in range(1, total + 1):
         for coin in coins:
-            if coin <= i:
+            if coin <= k:
                 arr[k] = min(arr[k], 1 + arr[k - coin])
 
     if arr[total] == float('inf'):
