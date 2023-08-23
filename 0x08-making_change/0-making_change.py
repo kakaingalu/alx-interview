f7451ce1a5ac333ffa5183b36dbea03b72fcb323
wantonraven <josephngalu96@gmail.com>
2023-08-23 08:40:17 +0300

    solve pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,33 +2,30 @@
 """generates change
 """
 
+
 def makeChange(coins, total):
-    """A function that determines the 
-    fewest number of coins needed to 
+    """A function that determines the
+    fewest number of coins needed to
     meet a given amount total
-    
     Args:
-        coins (_type_): a list of the values 
+        coins (_type_): a list of the values
         of the coins in your possession
         total (_type_): total amount
-        
     return:
-           fewest number of coins needed 
+           fewest number of coins needed
            to meet total
     """
     if total == 0 or total < 0:
         return 0
-    
     arr = [float('inf')] * (total + 1)
     arr[0] = 0
-    
+
     for k in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 arr[k] = min(arr[k], 1 + arr[k - coin])
-    
+
     if arr[total] == float('inf'):
         return -1
-    else: 
+    else:
         return arr[total]
-    
